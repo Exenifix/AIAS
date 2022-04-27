8abae2daf8df50c22a4ee6d1b233ceef3e0c4ed0
Exenifix <yeligar@ya.ru>
2022-04-27 19:50:23 +0300

    Content listing is no longer required (>10k samples gathered)

--- ext/automod.py
+++ ext/automod.py
@@ -31,10 +31,11 @@ class Automod(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: disnake.Message):
-        if message.author.bot or len(message.content) == 0:
-            return
-        await self.bot.db.register_message(message.content)
-        if message.author.guild_permissions.manage_guild:
+        if (
+            message.author.bot
+            or len(message.content) == 0
+            or message.author.guild_permissions.manage_guild
+        ):
             return
 
         await self._process_message(message)
