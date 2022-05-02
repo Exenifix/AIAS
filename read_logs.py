01dd738dce2d2bb24022d5f3ac1fe508c1ac3709
Exenifix <yeligar@ya.ru>
2022-05-02 20:41:30 +0300

    Visual addition to logs reader

--- read_logs.py
+++ read_logs.py
@@ -1,8 +1,10 @@
 import subprocess
 
+print("Starting subprocess...")
 proc = subprocess.Popen(
     ["docker", "logs", "-f", "aias"], stderr=subprocess.PIPE, text=True
 )
+print("Fetching logs...")
 for line in iter(proc.stderr.readline, ""):
     print(line, end="")
     if "bot is ready!" in line.lower():
