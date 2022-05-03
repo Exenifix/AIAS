6f22517b588409ff70e9dc7bac74b1e8bbcbbb61
Exenifix <yeligar@ya.ru>
2022-05-03 15:58:31 +0300

    read_logs will now return 1 if the application was killed

--- read_logs.py
+++ read_logs.py
@@ -1,12 +1,13 @@
 import subprocess
+import sys
 
-print("Starting subprocess...")
 proc = subprocess.Popen(
     ["docker", "logs", "-f", "aias"], stderr=subprocess.PIPE, text=True
 )
-print("Fetching logs...")
 for line in iter(proc.stderr.readline, ""):
     print(line, end="")
     if "bot is ready!" in line.lower():
         proc.kill()
-        break
+        sys.exit(0)
+
+sys.exit(1)
