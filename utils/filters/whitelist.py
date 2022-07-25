f43264ebc689527c1da3dfdbbf029f347154efda
Exenifix <yeligar@ya.ru>
2022-07-25 10:43:03 +0300

    The whitelist now ignores emojis

--- utils/filters/whitelist.py
+++ utils/filters/whitelist.py
@@ -1,5 +1,8 @@
+from emoji import demojize
+
+
 def contains_fonts(allowed_symbols: str, content: str) -> tuple[bool, set[str]]:
     allowed_symbols = set(allowed_symbols)
     allowed_symbols.add(" ")
-    content = set(content.replace(" ", "").lower())
+    content = set(demojize(content.replace(" ", "").lower()))
     return not allowed_symbols >= content, content - allowed_symbols
