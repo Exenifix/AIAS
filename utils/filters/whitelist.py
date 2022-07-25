d4e839143c3e7ca210b81d30c017219c5a50bef0
Exenifix <yeligar@ya.ru>
2022-07-25 19:48:11 +0300

    [patch] Fixed whitelist blocking \n out

--- utils/filters/whitelist.py
+++ utils/filters/whitelist.py
@@ -2,7 +2,8 @@ from emoji import demojize
 
 
 def contains_fonts(allowed_symbols: str, content: str) -> tuple[bool, set[str]]:
+    content = content.strip().replace("\n", "").replace(" ", "").lower()
     allowed_symbols = set(allowed_symbols)
     allowed_symbols.add(" ")
-    content = set(demojize(content.replace(" ", "").lower()))
+    content = set(demojize(content))
     return not allowed_symbols >= content, content - allowed_symbols
