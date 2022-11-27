195f80b8473796e082b5ca11d427f1dadda5fff8
Exenifix <yeligar@ya.ru>
2022-11-27 12:04:38 +0300

    [patch] Fixed bad whitelist processing (closes #77)

--- utils/filters/whitelist.py
+++ utils/filters/whitelist.py
@@ -1,9 +1,9 @@
 from emoji import demojize
 
 
-def contains_fonts(allowed_symbols: str, content: str) -> tuple[bool, set[str]]:
+def contains_fonts(allowed_symbols: str, content: str) -> tuple[bool, list[str]]:
     content = content.strip().replace("\n", "").replace(" ", "").lower()
     allowed_symbols = set(allowed_symbols)
     allowed_symbols.add(" ")
     content = set(demojize(content))
-    return not allowed_symbols >= content, content - allowed_symbols
+    return not allowed_symbols >= content, list(content - allowed_symbols)
